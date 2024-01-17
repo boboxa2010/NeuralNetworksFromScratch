5cbc6fd167a2578a341a9de01c46154f2f221da6
boboxa2010 <ivanboboshko888@gmail.com>
2024-01-17 23:41:04 +0300

    поправил артефакт

--- MSE.cpp
+++ MSE.cpp
@@ -10,19 +10,3 @@ Eigen::RowVectorXd MSE::GetGradient(const Eigen::VectorXd &predicted, const Eige
     return 2 * (predicted - answer);
 }
 
-#pragma once
-
-#include "Eigen/Eigen"
-
-class MSE {
-public:
-    MSE() = default;
-
-    double operator()(const Eigen::VectorXd &x, const Eigen::VectorXd &y) const;
-
-    Eigen::RowVectorXd GetGradient(const Eigen::VectorXd &predicted,
-                                   const Eigen::VectorXd &answer) const;
-
-private:
-    std::function<double(double, double)> derivative_;
-};
