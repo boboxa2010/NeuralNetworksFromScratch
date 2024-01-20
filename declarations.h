f97913b4464aa19815bf1f6cbb6a4f501ee05a69
boboxa2010 <ivanboboshko888@gmail.com>
2024-01-21 00:10:38 +0300

    add declarations

---
+++ declarations.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Eigen/Dense"
+#include "EigenRand/EigenRand"
+
+namespace project {
+    using NumT = double;
+    using Vector = Eigen::VectorXd;
+    using RowVector = Eigen::RowVectorXd;
+    using Matrix = Eigen::MatrixXd;
+    using RandomSeed = Eigen::Rand::P8_mt19937_64;
+}
