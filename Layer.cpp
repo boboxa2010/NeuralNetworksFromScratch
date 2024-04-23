1888de37083510d39c87d71971dd225524194b5d
boboxa2010 <ivanboboshko888@gmail.com>
2024-04-23 18:27:44 +0300

    add func to layer

--- Layer.cpp
+++ Layer.cpp
@@ -3,50 +3,41 @@
 #include "utils.h"
 
 namespace nn {
-Layer::Layer(Input input, Output output, ActivationFunction f)
-    : function_(std::move(f)),
-      weights_(GenerateRandNMatrix(output.size, input.size)),
-      bias_(GenerateRandNVector(output.size)) {
-}
-
-Layer::Layer(Input input, Output output, const ActivationFunction &f)
-    : function_(f),
-      weights_(GenerateRandNMatrix(output.size, input.size)),
-      bias_(GenerateRandNVector(output.size)) {
-}
-
-Vector Layer::Evaluate(const Vector &x) const noexcept {
+Vector Layer::Evaluate(const Vector &x) const {
     assert(x.size() == weights_.cols());
-    return function_->ApplyFunction(weights_ * x + bias_);
+    return function_->Evaluate(weights_ * x + bias_);
 }
 
-Vector Layer::EvaluateDerivative(const Vector &x) const noexcept {
-    assert(x.size() == weights_.cols());
-    return function_->ApplyDerivative(weights_ * x + bias_);
-}
-
-Matrix Layer::GetWeightsGradient(const Vector &x, const RowVector &u) const noexcept {
-    return GetBiasGradient(x, u) * x.transpose();
-}
-
-Vector Layer::GetBiasGradient(const Vector &x, const RowVector &u) const noexcept {
-    assert(x.size() == weights_.cols());
-    assert(u.size() == weights_.rows());
-    return function_->GetDifferential(weights_ * x + bias_) * u.transpose();
-}
-
-Vector Layer::GetNextGradient(const Vector &x, const RowVector &u) const noexcept {
+RowVector Layer::GetNextGradient(const Vector &x, const RowVector &u) const {
     assert(x.size() == weights_.cols());
     assert(u.size() == weights_.rows());
     return u * function_->GetDifferential(weights_ * x + bias_) * weights_;
 }
 
-void Layer::Update(const Matrix &weights_grad, const Vector &bias_grad,
-                   LearningRate &learning_rate) noexcept {
-    assert(weights_grad.rows() == weights_.rows() && weights_grad.cols() == weights_.cols());
-    assert(bias_grad.size() == bias_.size());
+void Layer::Update(LearningRate &learning_rate) {
     Scalar lr = learning_rate();
-    weights_ -= lr * weights_grad;
-    bias_ -= lr * bias_grad;
+    weights_ -= lr * grad_weights_;
+    bias_ -= lr * grad_bias_;
+}
+
+RowVector Layer::BackPropagation(const Vector &x, const RowVector &u) {
+    grad_weights_ += GetWeightsGradient(x, u);
+    grad_bias_ += GetBiasGradient(x, u);
+    return GetNextGradient(x, u);
+}
+
+void Layer::ZeroGrad() {
+    grad_weights_.setZero();
+    grad_bias_.setZero();
+}
+
+Matrix Layer::GetWeightsGradient(const Vector &x, const RowVector &u) const {
+    return GetBiasGradient(x, u) * x.transpose();
+}
+
+Vector Layer::GetBiasGradient(const Vector &x, const RowVector &u) const {
+    assert(x.size() == weights_.cols());
+    assert(u.size() == weights_.rows());
+    return function_->GetDifferential(weights_ * x + bias_) * u.transpose();
 }
 }  // namespace nn
--- Layer.h
+++ Layer.h
@@ -2,44 +2,51 @@
 
 #include "ActivationFunctions.h"
 #include "LearningRate.h"
+#include "utils.h"
 
 namespace nn {
 struct Input {
     explicit Input(size_t size) : size(size) {
     }
-
-    size_t size;
+    Index size;
 };
 
 struct Output {
     explicit Output(size_t size) : size(size) {
     }
-
-    size_t size;
+    Index size;
 };
 
 class Layer {
 public:
-    Layer(Input input_size, Output output_size, ActivationFunction f);
+    template <typename T>
+    Layer(Input input, Output output, T &&f)
+        : function_(std::move(f)),
+          weights_(GenerateRandNMatrix(output.size, input.size)),
+          bias_(GenerateRandNVector(output.size)),
+          grad_weights_(Matrix::Zero(output.size, input.size)),
+          grad_bias_(Matrix::Zero(output.size, 1)) {
+    }
 
-    Layer(Input input_size, Output output_size, const ActivationFunction &f);
+    Vector Evaluate(const Vector &x) const;
 
-    Vector Evaluate(const Vector &x) const noexcept;
+    RowVector GetNextGradient(const Vector &x, const RowVector &u) const;
 
-    Vector EvaluateDerivative(const Vector &x) const noexcept;
+    void Update(LearningRate &learning_rate);
 
-    Matrix GetWeightsGradient(const Vector &x, const RowVector &u) const noexcept;
+    RowVector BackPropagation(const Vector &x, const RowVector &u);
 
-    Vector GetBiasGradient(const Vector &x, const RowVector &u) const noexcept;
-
-    Vector GetNextGradient(const Vector &x, const RowVector &u) const noexcept;
-
-    void Update(const Matrix &weights_grad, const Vector &bias_grad,
-                LearningRate &learning_rate) noexcept;
+    void ZeroGrad();
 
 private:
+    Matrix GetWeightsGradient(const Vector &x, const RowVector &u) const;
+
+    Vector GetBiasGradient(const Vector &x, const RowVector &u) const;
+
     Matrix weights_;
     Vector bias_;
+    Matrix grad_weights_;
+    Vector grad_bias_;
     ActivationFunction function_;
 };
 }  // namespace nn
